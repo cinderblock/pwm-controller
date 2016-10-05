fba31e5ed1972896fe4f48d41dbc578223f66828
Cameron Tacklind <cameron@tacklind.com>
2016-10-05 01:51:19 -0700

    First version ready to test

--- src/client/PWMController.jsx
+++ src/client/PWMController.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import ReactSlider from 'react-slider';
+
+import socketIO from 'socket.io-client';
+
+import config from '../config.js';
+
+// TODO: Parametrize this...
+var id = 'demo';
+
+const MAXslider = 100 * 1000;
 
 export default class PWMController extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      duty: 10*1000,
+      period: 20*1000,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    if (event[0] != this.state.duty) {
+      this.setState({
+        duty: event[0],
+      });
+      this.io.emit('command', {id, type: 'duty', data: event[0] * 1000});
+    }
+    if (event[1] != this.state.period) {
+      this.setState({
+        period: event[1],
+      });
+      this.io.emit('command', {id, type: 'period', data: event[1] * 1000});
+    }
+  }
+  componentDidMount() {
+    this.io = new socketIO(config.client.serverOrigin);
+    this.io.emit('command', {id, type:'enable'});
   }
   render() {
+    var perc = this.state.duty / this.state.period;
+    var hz = 1 / (this.state.period);
+
     return (
-      <div className="PWMController"></div>
+      <div className="PWMController">
+        <ReactSlider withBars snapDragDisabled
+          value={[this.state.duty, this.state.period]}
+          min={0} max={MAXslider}
+          onChange={this.handleChange}
+        >
+          <div className="duty">{(perc*100).toFixed(2)}%</div>
+          <div className="period">Hz</div>
+        </ReactSlider>
+      </div>
     );
   }
 };
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var http = require('http');
 var socketIO = require('socket.io');
+var pwm = require('pwm');
 
 var config = require('./config.js');
 var pwms = require(config.pwmsFile);
@@ -12,8 +13,27 @@ let app = express();
 let server = http.Server(app);
 let io = socketIO(server);
 
+for (var id in pwms) {
+  if (!pwms.hasOwnProperty(id)) continue;
+  pwms[id].device = pwm.export(pwms[id].channel, pwms[id].channel, () => {
+    io.sockets.emit('pwmId', id);
+  });
+}
+
 io.on('connection', socket => {
-  console.log('a user connected');
+  socket.on('command', command => {
+    console.log(command);
+    if (!command.id || !pwms[command.id]) return;
+    if (command.type == 'reset') {
+      pwms[command.id].device.reset();
+    } else if (command.type == 'enable') {
+      pwms[command.id].device.setEnable(command.data, () => {});
+    } else if (command.type == 'period') {
+      pwms[command.id].device.setPeriod(command.data, () => {});
+    } else if (command.type == 'duty') {
+      pwms[command.id].device.setDutyCycle(command.data, () => {});
+    }
+  });
 });
 
 app.get('/', function (req, res) {
